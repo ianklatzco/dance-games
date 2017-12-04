bd0e78fe42d6443b90da6b1029877ad174536fec
Ian Klatzco <iklatzco@gmail.com>
2017-12-04 01:03:47 -0600

    add better-scorecalc.py

---
+++ better-scorecalc.py
@@ -0,0 +1,24 @@
+import sys
+# w1 w2 w3 w4 w5 holds_held mines_hit rolls_held
+ratings = [490,90,6,4,0]
+holds = []
+mines = [] # hit
+total_notes = sum(ratings)
+print(total_notes)
+
+weights = [5,4.666666666,4,3,0] 
+
+for rating,weight in ratings,weights:
+	values.append()
+
+fplus = fplus * 5
+f = f * 4.6666666666666
+ex = ex * 4
+great = great * 3
+miss = miss * 0
+
+
+print (  (fplus + f + ex + great)/ ( total*5)  ) 
+
+# 98.2 , 98.41
+# 98.5
